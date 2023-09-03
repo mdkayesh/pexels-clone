ba563ee19ddeb56b58eb0324fd8558d15f2362db
KayesKhanAkash <kayeskhanakash@gmail.com>
2023-09-03 21:56:12 +0600

    2nd commit

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -4,10 +4,7 @@ type classProps = {
   className: string;
 };
 
-// <!-- Generator: Adobe Illustrator 25.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-
 const Logo = ({ className }: classProps) => {
-  // enable-background:new 0 0 1378 984.9
   return (
     <div className={className}>
       <SiPexels className={"w-full h-full fill-primary"} />
